19023ea6d6c166f8d6bbcb2f95bf1c673aaf4a25
calum-atkins <calum2atkins@gmail.com>
2021-11-01 14:15:46 +0000

    Added profile seeder
    Seeds one row

---
+++ database/seeders/ProfileTableSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Profile;
+use Illuminate\Database\Seeder;
+
+class ProfileTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $p = new Profile();
+        $p->firstname = "James";
+        $p->surname = "Jones";
+        $p->save();
+    }
+}
