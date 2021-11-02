aeda5a5d1e580f54c452f63003d1f163ab392c43
calum-atkins <calum2atkins@gmail.com>
2021-11-02 11:14:14 +0000

    Added group model
    Used to show the name, description and admin (profile_id) of the group

---
+++ database/migrations/2021_11_02_110303_create_groups_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateGroupsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('groups', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('description');
+            $table->bigInteger('profile_id')->unsigned();
+            $table->timestamps();
+
+            $table->foreign('profile_id')->references('id')->
+                on('profiles')->onDelete('cascade')->onUpdate('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('groups');
+    }
+}
