01a495648b5d82caefdc10428924acbbd8da48df
calum-atkins <calum2atkins@gmail.com>
2021-12-14 10:31:46 +0000

    Updated create post view for better UI format

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -5,15 +5,24 @@
 @endsection
 
 @section('content')
-    <form method="POST" action="{{ route('posts.store') }}">
-        @csrf
-        <p>Title: <input type="text" name="title"></p>
-        <p>Content: <input type="text" name="contents"></p>
-        <p>User ID: <input type="text" name="user_id"></p>
-        <p>Group ID: <input type="text" name="group_id"></p>
-        <input type="submit" value="Submit">
-        <a href="{{ route('posts.index') }}">Cancel</a>
-
-    </form>
-
+<div class="row justify-content-center">
+    <div class="col-md-4">
+        <div class="card mt-5">
+            <div class="card-header">
+                    Create New Post
+            </div>
+            <div class="card-body">
+                <form method="POST" action="{{ route('posts.store') }}">
+                @csrf
+                <p>Title: <input type="text" name="title" class="form-control"></p>
+                <p>Content: <input type="text" name="contents" class="form-control"></p>
+                <p>User ID: <input type="text" name="user_id" class="form-control"></p>
+                <p>Group ID: <input type="text" name="group_id" class="form-control"></p>
+                <input type="submit" value="Submit" class="form-control bg-success">
+                <a href="{{ route('posts.index') }}" class="form-control bg-danger text-center">Cancel</a>
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
 @endsection
